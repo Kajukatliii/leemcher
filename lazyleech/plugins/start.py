22b7b085a88550861b4004ab0d4732e41b0626b5
Black hat hacker <95142447+Kajukatliii@users.noreply.github.com>
2022-03-23 04:35:18 -0700

    Update start.py

--- lazyleech/plugins/start.py
+++ lazyleech/plugins/start.py
@@ -17,6 +17,6 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command('start'))
 async def start_cmd(client, message):
-    await message.reply_text('''<b>Hi, I am Torrent Lazyleech.</b>\n
+    await message.reply_text('''<b>Hi, I am Alchemist Leecher bot.</b>\n
 I can leech direct / torrent links.\n
-For More Info /help)'''.strip())
+Powered by - @AlchemistNetwork)'''.strip())
